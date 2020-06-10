a7ec2713f53ff38f197dcc0a6d8cd2835e687ba3
Ayodeji Odukoya <ayodeji.od@gmail.com>
2020-06-11 00:15:00 +0100

    fixed init trans

--- src/Concerns/ManagesTransaction.php
+++ src/Concerns/ManagesTransaction.php
@@ -2,6 +2,7 @@
 
 namespace Cuitcode\Paystack\Concerns;
 
+use Carbon\Carbon;
 use Cuitcode\Paystack\Paystack;
 use Cuitcode\Paystack\Exceptions\CustomerAlreadyCreated;
 use Cuitcode\Paystack\Exceptions\InvalidArgument;
@@ -31,7 +32,49 @@ trait ManagesTransaction
      */
      public function initializeTransaction(array $options = [])
      {
-        $transaction =  new Transaction;
+        // void previous access codes.
+        $transaction =  $this->transactions()->where("status","pending")->first();
+        $transaction->status = "unused";
+        $transaction->save();
+
+        
+
+        $this->transactions->filter(function (Transaction $transaction) {
+            return $transaction->reference === $data['reference'];
+        })->each(function (Transaction $transaction) use ($data, $user) {
+
+            // Transaction Data...
+            $transaction->user_id = $user->id;
+            $transaction->paystack_id = $data['id'] ?? null;
+            $transaction->status = $data['status'] ?? null;
+            $transaction->gateway_response = $data['gateway_response'] ?? null;
+            $transaction->plan_code = $data['plan']['plan_code'] ?? null;
+            $transaction->amount = $data['plan']['amount'] / 100 ?? null;
+            $transaction->paid_at = Carbon::createFromTimestamp($data['paid_at']);
+
+            $transaction->save();
+
+            // Update transaction authorization...
+            if (isset($data['authorization'])) {
+                $authorization = $data['authorization'];
+
+                $transaction->authorization()->updateOrCreate([
+                    'code' => $authorization['authorization_code'],
+                ], [
+                    'channel' => $authorization['channel']?? null,
+                    'country_code' => $authorization['country_code']?? null,
+                    'reusable' => $authorization['reusable']?? null,
+                    'card_type' => $authorization['card_type']?? null,
+                    'bin' => $authorization['bin']?? null,
+                    'last_four' => $authorization['last4']?? null,
+                    'exp_month' => $authorization['exp_month']?? null,
+                    'exp_year' => $authorization['exp_year']?? null,
+                    'brand' => $authorization['brand']?? null,
+                    'bank' => $authorization['bank']?? null,
+                    'signature' => $authorization['signature']?? null,
+                ]);
+            }
+        });
  
          if (! array_key_exists('email', $options) && $email = $this->paystackEmail()) {
              $options['email'] = $email;
@@ -45,12 +88,14 @@ trait ManagesTransaction
             $options['amount'] = 0;
          }
  
-         // Here we will create the customer instance on Paystack and store the ID of the
-         // user from Paystack. This ID will correspond with the Paystack user instances
-         // and allow us to retrieve users from Paystack later when we need to work.
-         $trans = PaystackTransaction::initialize(
-             $options, $this->paystackOptions()
-         );
+        // Here we will create the customer instance on Paystack and store the ID of the
+        // user from Paystack. This ID will correspond with the Paystack user instances
+        // and allow us to retrieve users from Paystack later when we need to work.
+        $trans = PaystackTransaction::initialize(
+            $options, $this->paystackOptions()
+        );
+
+        $transaction =  new Transaction;
 
         $this->transactions()->create([
             "access_code" => $trans["data"]->access_code,
